e5030385b9bb6f69c00f59e07cbabd7e0fd3972c
Kitkamseng <64192497+Kitkamseng@users.noreply.github.com>
2023-07-17 20:42:17 +0800

    Log in page done

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './LoginPage.css';
+
+function LoginPage() {
+  return (
+    <div className='main-container'>
+       <h1>Log In</h1>
+       <form method='post'>
+        <div class='txt_field'>
+          <input type='text' required />
+          <span></span>
+          <label>Username</label>
+        </div>
+        <div class='txt_field'>
+          <input type='text' required />
+          <span></span>
+          <label>Password</label>
+        </div>
+        <div class='pass'>Forgot Password?</div>
+        <input type='submit' value="Login" />
+        <div class='signup_link'>
+          Not a member? <a href='#'>Sign Up</a>
+        </div>
+       </form>
+    </div>
+  );
+} 
+
+export default LoginPage;
