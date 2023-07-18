e690a20e692715eefd94fa465d1f7604d2ba7bad
Kitkamseng <64192497+Kitkamseng@users.noreply.github.com>
2023-07-18 17:41:57 +0800

    Nav Bar base done

---
+++ src/components/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import './Header.css';
+import VoteIcon from '../images/VoteIcon.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocketchat } from '@fortawesome/free-brands-svg-icons';
+import { faBell, faUser } from '@fortawesome/free-regular-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
+
+function Header(){
+
+    let navigate = useNavigate();
+
+    const testOne = () => {
+        navigate('/login');
+    }
+
+    return(
+        <div className="header-container">
+            {/* <img 
+                src={VoteIcon} 
+                className="header-icon"
+            /> */}
+            <div className="header-icon"> 
+                Logo
+            </div>
+            <div className="header-name">
+                VOTE
+            </div>
+            
+            <div className="search-bar-container">
+                <input 
+                    className="search-bar"
+                    placeholder="Search..."
+                />
+            </div>
+
+            <div className="personal-setting">
+                <div className="personal-box">
+                    <FontAwesomeIcon 
+                        icon={faRocketchat} 
+                        size="3x" 
+                        onClick={testOne}
+                    />
+                </div>
+                <div className="personal-box">
+                    <FontAwesomeIcon icon={faBell} size="3x"/>
+                </div>
+                <div className="personal-box">
+                    <FontAwesomeIcon icon={faUser} size="3x" />
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default Header;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './HomePage.css';
+import Header from './../components/Header';
 
 function HomePage(){
     return(
-        <div>
-            hello
-        </div>
+        <>
+        <Header />
+        </>
+
     );
 }
 
