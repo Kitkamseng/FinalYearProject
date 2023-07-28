16d4dd83db08cf3793fd81fda67caa03aeb6d430
Kit Kam Seng <Kitkamseng96@gmail.com>
2023-07-28 14:13:36 +0800

    Base backend done

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,14 +2,35 @@ import './SignUpPage.css';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
+import { async } from 'q';
+import axios from 'axios';
 
 
-function LoginPage() {
+function SignUpPage() {
 
   let navigate = useNavigate();
 
-  const [password, setPassword] = useState("");
-  const [visible, setVisible] = useState(false);
+  const [user, setUser] = useState({
+    email:"",
+    username:"",
+    password:""
+  })
+
+  const{email, username, password} = user
+
+  const onInputChange =(e) =>{
+    setUser({...user, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/user", user);
+    navigate("/");
+    console.log(e);
+  }
+
+  // const [password, setPassword] = useState("");
+  // const [visible, setVisible] = useState(false);
 
   const handleLogInClick = () => {
     navigate('/login');
@@ -18,24 +39,54 @@ function LoginPage() {
   return (
     <div className='signup-container'>
        <h1>Sign Up</h1>
-       <form method='post'>
+       <form 
+        onSubmit={(e) => onSubmit(e)}
+       >
        <div class='txt_field'>
-          <input type='text' required />
+          <input 
+            type={"text"}
+            className='form-control'
+            placeholder='Enter Your Email'
+            name='email'
+            value={email}
+            onChange={(e)=>onInputChange(e)}
+          />
           <span></span>
           <label>Email</label>
         </div>
         <div class='txt_field'>
-          <input type='text' required />
+          <input 
+            type={"text"}
+            className='form-control'
+            placeholder='Enter Your UserName'
+            name='username'
+            value={username}
+            onChange={(e)=>onInputChange(e)}
+          />
           <span></span>
           <label>Username</label>
         </div>
 
         <div class='txt_field'>
           <input 
+            type={"text"}
+            className='form-control'
+            placeholder='Enter Your Password'
+            name='password'
             value={password}
-            type={visible ? "text" : "password"}
+            onChange={(e)=>onInputChange(e)}
+          />
+          <span></span>
+          <label>Password</label>
+        </div>
+
+        {/* <div class='txt_field'>
+          <input 
+            value={newPassword}
+            onChange={(e) => onInputChange(e)}
+            type={visible ? "text" : "newPassword"}
             id="password"
-            onChange={e => setPassword(e.target.value)}
+            // onChange={e => setPassword(e.target.value)}
           />
           <label>Password</label>
           <div className='eye-toggle' onClick={() => setVisible(!visible)}>
@@ -43,7 +94,7 @@ function LoginPage() {
               visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
             }
           </div>
-        </div>
+        </div> */}
         
         <input type='submit' value="Register" />
 
@@ -55,4 +106,4 @@ function LoginPage() {
   );
 } 
 
-export default LoginPage;
+export default SignUpPage;
