d48b1bacf2ce400f6c1c5332d6baac5a1d03e342
Kit Kam Seng <Kitkamseng96@gmail.com>
2023-09-06 13:58:13 +0800

    Profile page bio and other stuff added

---
+++ src/components/AddProjectCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './AddProjectCard.css';
+import PostCreation from './../images/Post-creation.png'
+import { useNavigate } from "react-router-dom";
+
+
+function AddProjectCard () {
+
+    let navigate = useNavigate();
+
+    const handleAddCard = () =>{
+        navigate('/selectionpage')
+    }
+
+    return(
+        <>
+            <div className="add-project-container">
+                <div 
+                    className="add-card-container"
+                    onClick={handleAddCard}
+                >
+                    <div className="project-add-icon">
+                        <img src={PostCreation} />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+
+
+}
+
+export default AddProjectCard;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,6 +5,7 @@ import Header from './../components/Header';
 import ProjectCard from "../components/ProjectCard";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import AddProjectCard from "../components/AddProjectCard";
 
 function ProfilePage(){
 
@@ -47,10 +48,54 @@ function ProfilePage(){
                             Edit Profile
                         </button>
                     </div>
+                    <div className="project-bio-display">
+                        <div className="follower-stats-container">
+                            <div className="follower-container">
+                                <div className="follower-title">
+                                    Follower:
+                                </div>
+                                <div className="follower-count">
+                                    1.6m followers
+                                </div>
+                            </div>
+                            <div className="follower-container">
+                                <div className="follower-title">
+                                    Following:
+                                </div>
+                                <div className="follower-count">
+                                    1m following
+                                </div>
+                            </div>
+                            <div className="follower-container">
+                                <div className="follower-title">
+                                    Posts:
+                                </div>
+                                <div className="follower-count">
+                                    1 post
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bio-display-box">
+                            Meet Alex, a passionate creator driven by his love for 
+                            coding and building things. With an insatiable curiosity 
+                            and a knack for problem-solving, Alex finds his true calling 
+                            in the world of programming. From a young age, he was captivated 
+                            by the art of coding, and that fascination has only grown stronger 
+                            over the years.
+                        </div>
+                    </div>
                 </div>
                 <div className="profile-project-container">
                     <div className="profile-project-title">
                         To Do: 
+                    </div>
+                    <div className="project-space-container">
+                        <ProjectCard />
+                    </div>
+                </div>
+                <div className="profile-project-container">
+                    <div className="profile-project-title">
+                        Personal Project: 
                     </div>
                     <div className="project-space-container">
                         <ProjectCard />
